3222048f819178e92ce25d581d7ecd368772e5de
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-06-12 03:39:53 +0700

    disable .toprc on setup file

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 ln -sf ~/.dotfiles/bash/.bashrc ~/.bashrc
 
 # setup top(1)
-ln -sf ~/.dotfiles/top/.toprc ~/.toprc
+# ln -sf ~/.dotfiles/top/.toprc ~/.toprc
 
 # setup git
 ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
