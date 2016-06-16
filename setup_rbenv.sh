729c5db50249fc149105437ef21951b6fcc09b8c
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-06-16 10:45:11 +0700

    add ruby-build plugin

--- setup_rbenv.sh
+++ setup_rbenv.sh
@@ -5,5 +5,6 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' > ~/.dotfiles/rbenv/rbenv.path
 echo 'eval "$(rbenv init -)"' >> ~/.dotfiles/rbenv/rbenv.path
 exec $SHELL
 type rbenv
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo "Finished."
 echo "Run rbenv install -l to see available version"
